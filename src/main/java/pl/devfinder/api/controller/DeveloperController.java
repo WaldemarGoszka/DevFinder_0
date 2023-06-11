79b08d967db0c00527a244a73ef14b5cdbab6bdb
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-06-11 13:19:05 +0200

    create basic web structure and buttons link to endpoints

--- src/main/java/pl/devfinder/api/controller/DeveloperController.java
+++ src/main/java/pl/devfinder/api/controller/DeveloperController.java
@@ -1,16 +1,30 @@
 package pl.devfinder.api.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
+@AllArgsConstructor
 public class DeveloperController {
 
     public static final String DEVELOPER = "/developer";
+    public static final String DEVELOPER_PROFILE = "/developer/profile";
+    public static final String DEVELOPER_EDIT_PROFILE = "/developer/edit_profile";
+    public static final String OFFERS_LIST = "/developer/offers";
+    public static final String EMPLOYERS_LIST = "/developer/employers";
+    public static final String DEVELOPER_SETTINGS = "/developer/settings";
+    public static final String DEVELOPER_MATCHED_OFFERS = "/developer/matched_offers";
 
     @GetMapping(value = DEVELOPER)
     public String homePage(Model model) {
 
         return "developer_portal";
+    }
+    @GetMapping(value = DEVELOPER_PROFILE)
+    public String getProfile(Model model) {
+// getDeveloperProfile
+        return "developer_profile";
     }
 
 }
--- src/main/java/pl/devfinder/api/controller/EmployerController.java
+++ src/main/java/pl/devfinder/api/controller/EmployerController.java
@@ -1,15 +1,34 @@
 package pl.devfinder.api.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+@AllArgsConstructor
 public class EmployerController {
 
     public static final String EMPLOYER = "/employer";
+    public static final String EMPLOYER_PROFILE = "/employer/profile";
+    public static final String EMPLOYER_EDIT_PROFILE = "/employer/edit_profile";
+    public static final String MATCHED_DEVELOPERS = "/employer/matched_developers";
+    public static final String SEARCH_DEVELOPER =  "/employer/find_developer";
+    public static final String POST_A_JOB =  "/employer/post_job";
+    public static final String EMPLOYER_SETTINGS =  "/employer/settings";
+    public static final String EMPLOYEE = "/employer/employee";
 
     @GetMapping(value = EMPLOYER)
     public String homePage(Model model) {
 
         return "employer_portal";
     }
+
+    @GetMapping(value = EMPLOYER_PROFILE)
+    public String getProfile(Model model) {
+// getEmpoyerProfile
+        return "employer_profile";
+    }
+
+
 }
