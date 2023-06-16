46ba13138ec5e2c8b7dbbf15332ffa5f49eb99dc
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-06-16 21:16:42 +0200

    add my_posted_job_offers in employer portal

--- src/main/java/pl/devfinder/api/controller/EmployerController.java
+++ src/main/java/pl/devfinder/api/controller/EmployerController.java
@@ -15,6 +15,8 @@ public class EmployerController {
     public static final String MATCHED_DEVELOPERS = "/employer/matched_developers";
     public static final String SEARCH_DEVELOPER = "/employer/find_developer";
     public static final String POST_A_JOB = "/employer/post_job";
+
+    public static final String MY_JOB_OFFERS = "/employer/my_job_offers";
     public static final String EMPLOYER_SETTINGS = "/employer/settings";
     public static final String EMPLOYEE = "/employer/employee";
 
